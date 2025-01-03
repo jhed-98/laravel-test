83c570ac2170bfbf3cc972e6556ae651888373d2
jhed-98 <cod.mobile.jhed@gmail.com>
2025-01-02 23:10:19 -0500

    funcionamiento de jwt

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,16 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 Route::apiResource('tasks', TaskController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+
+// Validar los accesos  de los api con Tokens
+Route::get('/prueba',function(){
+    $user = JWTAuth::parseToken()->authenticate();
+    return $user;
+});
